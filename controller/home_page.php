54e94c66a6d6977fa90c731d8e79d003b41f83af
Julie.Canevet <julie.canevet@hotmail.fr>
2017-09-27 16:23:49 +0200

    add password protection for home_page

--- controller/home_page.php
+++ controller/home_page.php
@@ -1,8 +1,18 @@
 <?php 
-session_start();
-include '../model/home_page_post.php';
-include '../vue/header_vue.php'; // ajout du header page d'accueil
 
-$project = home_project();
+session_start();
+	ob_start();
+if(isset($_SESSION['name'])){
+	include '../model/home_page_post.php';
+	include '../vue/header_vue.php'; // ajout du header page d'accueil
+
+	$project = home_project();
 
 include '../vue/home_page_vue.php'; // ajout des derniers projets
+}
+else {
+
+echo 'veuillez vous inscrire ou vous connecter';
+header('Refresh: 2; index.php');
+ob_end_flush();
+}
